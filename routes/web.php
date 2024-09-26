61686ee83c4fd109b12b0c121f799243a98a56a4
MasterLenoX <lenoxemperado@gmail.com>
2024-09-26 21:08:02 +0800

    3rd commit - reupdating .env and Add routes and public folders

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+use \App\Http\Controllers\AuthController;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->name('auth.')->group(function(){
+
+    Route::middleware(['guest:web'])->group(function(){
+        Route::view('/login', 'backend.pages.auth.login')->name('login');
+        Route::view('/forgot-password','backend.pages.auth.forgot')->name('forgot-password');
+    });
+
+    Route::middleware([])->group(function(){
+        Route::view('/home',[AuthController::class, 'index'])->name('home');
+    });
 });
