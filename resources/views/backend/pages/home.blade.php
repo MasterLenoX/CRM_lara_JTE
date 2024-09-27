767fb07701bc2fb0e99d1bc360586c896dfc9a1c
MasterLenoX <lenoxemperado@gmail.com>
2024-09-27 15:10:58 +0800

    6th commit - creating pages-layout for header, sidebar, footer and forgot-password page

---
+++ resources/views/backend/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('backend.layouts.pages-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Home - Dashboard | CRM Laravel Tabler UI Kit')
+@section('content')
+
+  Home Dashboard Content page here...
+
+@endSection
