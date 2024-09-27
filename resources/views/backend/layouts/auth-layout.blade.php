6434b15a869bb0e688dc9523d1e60c8f85ded8c6
MasterLenoX <lenoxemperado@gmail.com>
2024-09-27 13:22:30 +0800

    5th commit - deleting AdminLTE admin template and creating a Login Form page

---
+++ resources/views/backend/layouts/auth-layout.blade.php
@@ -0,0 +1,63 @@
+{{-- Tabler admin template start --}}
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>@yield('pageTitle')</title>
+    <script defer data-api="/stats/api/event" data-domain="preview.tabler.io" src="/stats/js/script.js"></script>
+    <meta name="msapplication-TileColor" content="#066fd1"/>
+    <meta name="theme-color" content="#066fd1"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="mobile-web-app-capable" content="yes"/>
+    <meta name="HandheldFriendly" content="True"/>
+    <meta name="MobileOptimized" content="320"/>
+    <link rel="icon" href="./favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon"/>
+    <meta name="description" content="Tabler comes with tons of well-designed components and features. Start your adventure with Tabler and make your dashboard great again. For free!"/>
+    <meta name="canonical" content="https://tabler.io/demo/sign-in.html">
+    <meta name="twitter:image:src" content="https://tabler.io/demo/static/og.png">
+    <meta name="twitter:site" content="@tabler_ui">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Tabler: Premium and Open Source dashboard template with responsive and high quality UI.">
+    <meta name="twitter:description" content="Tabler comes with tons of well-designed components and features. Start your adventure with Tabler and make your dashboard great again. For free!">
+    <meta property="og:image" content="https://tabler.io/demo/static/og.png">
+    <meta property="og:image:width" content="1280">
+    <meta property="og:image:height" content="640">
+    <meta property="og:site_name" content="Tabler">
+    <meta property="og:type" content="object">
+    <meta property="og:title" content="Tabler: Premium and Open Source dashboard template with responsive and high quality UI.">
+    <meta property="og:url" content="https://tabler.io/demo/static/og.png">
+    <meta property="og:description" content="Tabler comes with tons of well-designed components and features. Start your adventure with Tabler and make your dashboard great again. For free!">
+    <!-- CSS files -->
+    <base href="/">
+    <link href="./backend/dist/css/tabler.min.css?1726507346" rel="stylesheet"/>
+    <link href="./backend/dist/css/tabler-flags.min.css?1726507346" rel="stylesheet"/>
+    <link href="./backend/dist/css/tabler-payments.min.css?1726507346" rel="stylesheet"/>
+    <link href="./backend/dist/css/tabler-vendors.min.css?1726507346" rel="stylesheet"/>
+    @stack('stylesheets')
+    <link href="./backend/dist/css/demo.min.css?1726507346" rel="stylesheet"/>
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      :root {
+      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+      }
+      body {
+      	font-feature-settings: "cv03", "cv04", "cv11";
+      }
+    </style>
+  </head>
+  <body  class=" d-flex flex-column">
+    <script src="./backend/dist/js/demo-theme.min.js?1726507346"></script>
+    @yield('content')
+    <!-- Libs JS -->
+    <!-- Tabler Core -->
+    <script src="./backend/dist/js/tabler.min.js?1726507346" defer></script>
+    @stack('scripts')
+    <script src="./backend/dist/js/demo.min.js?1726507346" defer></script>
+  </body>
+</html>
+{{-- Tabler admin template end --}}
+
